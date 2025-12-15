fcc980d8aef8f8c8814d625b7b3bec1741242a93
Gowtham602 <gowthamsundararajan7777@gmail.com>
2025-12-15 16:45:12 +0530

    working with serve

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,15 @@
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
 <!-- Page-specific scripts -->
-@stack('scripts')
+@push('scripts')
+<script>
+    const createStudentUrl = "{{ route('student.store') }}";
+    const studentsDataUrl  = "{{ route('student.list') }}";
+</script>
+
+<script src="{{ asset('js/student.js') }}"></script>
+@endpush
+
 
 </body>
 </html>
