baaaf9ab39b7eeb295fe0f7eef0b98afa7a0ca33
Gowtham602 <gowthamsundararajan7777@gmail.com>
2025-12-15 16:59:52 +0530

    web changes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Student;
 
-class studentController extends Controller
+class StudentController extends Controller
 {
     //
     public function index (Request $request){
