a700d9b6523da07fea172f66b68307243d86e1f0
Gowtham602 <gowthamsundararajan7777@gmail.com>
2025-12-15 16:22:45 +0530

    working with serve

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,34 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\studentController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
 
-Route::get('/', function () {
-    return view('students.index');
-});
-Route::get('/student',[studentController::class,'index'])->name('student.index');
-Route::post('/student/store',[studentController::class,'store'])->name('student.store');
-Route::get('/student/data',[studentController::class,'data'])->name('student.data');
-Route::delete('/student/delete/{id}',[studentController::class,'delete'])->name('student.delete');
-Route::get('/student/edit/{id}',[studentController::class,'edit'])->name('student.edit');
-Route::put('/student/update/{id}',[studentController::class,'studentUpdate'])->name('student.update');
-Route::get("/dashboard",[DashboardController::class,'index'])->name('dashboard.index');
-// Route::get("/student/softdeleted",[DashboardController::class,'softdeleted'])->name('dashboard.softdeleted');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
 
+Route::get('/', [DashboardController::class, 'index'])->name('home');
 
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard.index');
 
+Route::get('/student', [StudentController::class, 'index'])
+    ->name('student.index');
+
+Route::post('/student/store', [StudentController::class, 'store'])
+    ->name('student.store');
+
+Route::get('/student/data', [StudentController::class, 'data'])
+    ->name('student.data');
+
+Route::get('/student/edit/{id}', [StudentController::class, 'edit'])
+    ->name('student.edit');
+
+Route::put('/student/update/{id}', [StudentController::class, 'studentUpdate'])
+    ->name('student.update');
+
+Route::delete('/student/delete/{id}', [StudentController::class, 'delete'])
+    ->name('student.delete');
